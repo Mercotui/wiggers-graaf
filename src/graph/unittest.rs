a2c346cf6e07aa0e457a6278855ceea47f784e0c
Menno van der Graaf <mennovandergraaf@hotmail.com>
2024-12-10 00:58:39 +0100

    Add graph distance analysis

---
+++ src/graph/unittest.rs
@@ -0,0 +1,79 @@
+use crate::board::{get_start_board, make_move, Coordinates, SlideDirection, SlideMove};
+use crate::graph::{to_hash, Graph};
+
+fn init() {
+    let _ = env_logger::builder().is_test(true).try_init();
+}
+
+#[test]
+fn test_is_solution() {
+    init();
+    let mut graph: Graph = Graph::new();
+    
+    let board_1 = get_start_board();
+    let move_1 = SlideMove {
+        start: Coordinates { x: 1, y: 1 },
+        direction: SlideDirection::Down,
+    };
+    let board_2 = make_move(
+        &board_1,
+        &move_1,
+    ).expect("Failed to make move");
+    let move_2 = SlideMove {
+        start: Coordinates { x: 1, y: 0 },
+        direction: SlideDirection::Right,
+    };
+    let board_3 = make_move(
+        &board_2,
+        &move_2,
+    ).expect("Failed to make move");
+    let move_3 = SlideMove {
+        start: Coordinates { x: 2, y: 1 },
+        direction: SlideDirection::Left,
+    };
+    let board_4 = make_move(
+        &board_3,
+        &move_3,
+    ).expect("Failed to make move");
+    let move_4 = SlideMove {
+        start: Coordinates { x: 2, y: 0 },
+        direction: SlideDirection::Up,
+    };
+    // This should be the start board once again, we have swapped around to similar pieces
+    let board_5 = make_move(
+        &board_4,
+        &move_4,
+    ).expect("Failed to make move");
+    
+    assert_eq!(board_1, board_5);
+
+    graph.add_node(board_1);
+    graph.add_node(board_2);
+    graph.add_node(board_3);
+    graph.add_node(board_4);
+    // This should not have effect, as board_5 equals board_1
+    graph.add_node(board_5);
+
+    graph.add_edge(&board_1, &board_2, &move_1);
+    graph.add_edge(&board_2, &board_1, &move_1);
+    graph.add_edge(&board_2, &board_3, &move_2);
+    graph.add_edge(&board_3, &board_2, &move_2);
+    graph.add_edge(&board_3, &board_4, &move_3);
+    graph.add_edge(&board_4, &board_3, &move_3);
+    graph.add_edge(&board_4, &board_5, &move_4);
+    graph.add_edge(&board_5, &board_4, &move_4);
+    
+    graph.analyze(&board_1, &board_4);
+    
+    assert_eq!(graph.map.get(&to_hash(&board_1)).unwrap().distance_to_start.unwrap(), 0);
+    assert_eq!(graph.map.get(&to_hash(&board_2)).unwrap().distance_to_start.unwrap(), 1);
+    assert_eq!(graph.map.get(&to_hash(&board_3)).unwrap().distance_to_start.unwrap(), 2);
+    assert_eq!(graph.map.get(&to_hash(&board_4)).unwrap().distance_to_start.unwrap(), 1);
+    assert_eq!(graph.map.get(&to_hash(&board_5)).unwrap().distance_to_start.unwrap(), 0);
+    
+    assert_eq!(graph.map.get(&to_hash(&board_1)).unwrap().distance_to_solution.unwrap(), 1);
+    assert_eq!(graph.map.get(&to_hash(&board_2)).unwrap().distance_to_solution.unwrap(), 2);
+    assert_eq!(graph.map.get(&to_hash(&board_3)).unwrap().distance_to_solution.unwrap(), 1);
+    assert_eq!(graph.map.get(&to_hash(&board_4)).unwrap().distance_to_solution.unwrap(), 0);
+    assert_eq!(graph.map.get(&to_hash(&board_5)).unwrap().distance_to_solution.unwrap(), 1);
+}
--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,8 +1,12 @@
-use crate::board::{get_solved_board, get_start_board, get_valid_moves, is_solution, Board, Coordinates, SlideDirection, SlideMove};
+use crate::board::{
+    get_solved_board, get_start_board, get_valid_moves, is_solution, Board, Coordinates,
+    SlideDirection, SlideMove,
+};
 use crate::graph::Graph;
 
 pub struct Solver {
     pub graph: Graph,
+    start_board: Board,
     solution_node: Board,
 }
 
@@ -12,33 +16,63 @@ impl Solver {
         // Create graph
         let mut solver: Solver = Solver {
             graph: Graph::new(),
+            start_board: get_start_board(),
             solution_node: get_solved_board(),
         };
 
-        // Create process queue and initialize it with the start board
-        let mut inspection_queue: Vec<Board> = vec![get_start_board()];
+        // Add solution to graph
+        solver.graph.add_node(solver.solution_node);
 
-        while !inspection_queue.is_empty() {
-            // print!(".");
-            let board = inspection_queue
-                .pop()
-                .expect("Failed to pop from queue, is it empty?");
-            solver.graph.add_node(board);
+        // Find all possible moves from start
+        generate_moves(&mut solver);
 
-            get_valid_moves(&board)
-                .iter()
-                .for_each(|(slide_move, new_board)| {
-                    if !solver.graph.contains_node(new_board) {
-                        inspection_queue.push(*new_board)
-                    }
-                    solver.graph.add_edge(&board, new_board, &slide_move);
-                });
-            
-            if is_solution(&board) {
-                // TODO(Menno 13.11.2024) Add some fake SlideMove edge value and undo the pub on SlideMove and components
-                solver.graph.add_edge(&board, &solver.solution_node, &SlideMove{start: Coordinates { x: 1, y: 0 }, direction: SlideDirection::Down});
-            }
-        }
+        // Analyze the moves to find the distances from each node to start and solution
         solver
+            .graph
+            .analyze(&solver.start_board, &solver.solution_node);
+
+        solver
+    }
+}
+
+fn generate_moves(solver: &mut Solver) {
+    // Create process queue and initialize it with the start board
+    let mut inspection_queue: Vec<Board> = vec![solver.start_board.clone()];
+
+    while !inspection_queue.is_empty() {
+        // print!(".");
+        let board = inspection_queue
+            .pop()
+            .expect("Failed to pop from queue, is it empty?");
+        solver.graph.add_node(board);
+
+        get_valid_moves(&board)
+            .iter()
+            .for_each(|(slide_move, new_board)| {
+                if !solver.graph.contains_node(new_board) {
+                    inspection_queue.push(*new_board)
+                }
+                solver.graph.add_edge(&board, new_board, &slide_move);
+            });
+
+        if is_solution(&board) {
+            // TODO(Menno 13.11.2024) Add some fake SlideMove edge value and undo the pub on SlideMove and components
+            solver.graph.add_edge(
+                &board,
+                &solver.solution_node,
+                &SlideMove {
+                    start: Coordinates { x: 1, y: 0 },
+                    direction: SlideDirection::Down,
+                },
+            );
+            solver.graph.add_edge(
+                &solver.solution_node,
+                &board,
+                &SlideMove {
+                    start: Coordinates { x: 1, y: 0 },
+                    direction: SlideDirection::Down,
+                },
+            );
+        }
     }
 }
