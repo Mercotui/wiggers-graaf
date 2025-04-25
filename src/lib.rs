7e8e3db785cdbbd0de290efef726a5a10dd5990f
Menno van der Graaf <mennovandergraaf@hotmail.com>
2025-04-26 00:51:47 +0200

    Refactor rust lib interface into a struct
    This WiggersGraaf struct can be constructed from javascript as a class.
    Using a class instance will allow the transistion to a cleaner interface
    in follow-up patches.

--- src/lib.rs
+++ src/lib.rs
@@ -6,43 +6,44 @@ mod solver;
 mod utils;
 
 use crate::arrangement::Arrangement;
+use crate::board::BoardId;
 use crate::graph::Node;
 use crate::solver::Solver;
 use crate::utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 use web_sys::WebGl2RenderingContext;
-use crate::board::BoardId;
 
 #[wasm_bindgen]
-pub fn generate() -> Solver {
-    env_logger::init();
-
-    // let start = Instant::now();
-    let solver = Solver::new();
-    // let duration = start.elapsed();
-    // print!(
-    //     "Found {:?} states in {:?}",
-    //     solver.graph.node_count(),
-    //     duration
-    // );
-
-    solver
+struct WiggersGraaf {
+    solver: Solver,
 }
 
 #[wasm_bindgen]
-pub fn draw(canvas_id: &str, active_state: BoardId, solver: &Solver) -> Result<WebGl2RenderingContext, JsValue> {
-    set_panic_hook();
+impl WiggersGraaf {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        set_panic_hook();
+        env_logger::init();
+        Self {
+            solver: Solver::new(),
+        }
+    }
+    pub fn draw(
+        &mut self,
+        canvas_id: &str,
+        active_state: BoardId,
+    ) -> Result<WebGl2RenderingContext, JsValue> {
+        set_panic_hook();
 
-    let arrangement = Arrangement::new(&solver.graph, active_state);
-    renderer::draw(canvas_id, &arrangement)
-}
+        let arrangement = Arrangement::new(&self.solver.graph, active_state);
+        renderer::draw(canvas_id, &arrangement)
+    }
 
-#[wasm_bindgen]
-pub fn get_start_id() -> BoardId {
-    board::to_id(&board::get_start_board())
-}
+    pub fn get_start_id() -> BoardId {
+        board::to_id(&board::get_start_board())
+    }
 
-#[wasm_bindgen]
-pub fn get_state(solver: &Solver, id: BoardId) -> Node {
-    solver.graph.map.get(&id).expect("Invalid ID").clone()
+    pub fn get_state(&mut self, id: BoardId) -> Node {
+        self.solver.graph.map.get(&id).expect("Invalid ID").clone()
+    }
 }
