9f8089172f91ddfcb0f9d4e018dc5998662b71b3
Menno van der Graaf <mennovandergraaf@hotmail.com>
2025-05-04 23:32:05 +0200

    Refactor rendering code into GraphView
    Also switch from GLAM to Euclid to have clearer overview on
    coordinate spaces.

--- src/graph_view/renderer.rs
+++ src/graph_view/renderer.rs
@@ -1,48 +1,15 @@
-use crate::arrangement::Arrangement;
-use glam::{Affine2, FloatExt, Mat3, Vec2};
 use wasm_bindgen::{JsCast, JsValue};
 use web_sys::{
     HtmlCanvasElement, WebGl2RenderingContext, WebGlProgram, WebGlShader, WebGlUniformLocation,
     WebGlVertexArrayObject,
 };
 
-const SCALE_SPEED_FACTOR: f32 = 1.0;
-
 pub struct Renderer {
-    canvas: HtmlCanvasElement,
     gl: WebGl2RenderingContext,
     shaders: WebGlProgram,
-    // TODO(Menno 28.04.2025) These scale values could be converted to a (affine) transform matrix?
-    canvas_to_gl_scale_x: f32,
-    canvas_to_gl_scale_y: f32,
-    scale: f32,
-    translation: Vec2,
-    should_update_view_transform: bool,
     view_transform_location: WebGlUniformLocation,
     vao: WebGlVertexArrayObject,
     vertex_count: i32,
-}
-
-fn get_canvas(canvas_id: &str) -> Result<HtmlCanvasElement, JsValue> {
-    // Access DOM
-    let document = web_sys::window()
-        .ok_or(JsValue::from_str("Unable to access the window"))?
-        .document()
-        .ok_or(JsValue::from_str("Unable to access the DOM"))?;
-    // Get canvas element from DOM
-    document
-        .get_element_by_id(canvas_id)
-        .ok_or(JsValue::from_str(&format!(
-            "Could not find canvas: {}",
-            canvas_id
-        )))?
-        .dyn_into::<HtmlCanvasElement>()
-        .map_err(|_x| {
-            JsValue::from_str(&format!(
-                "Element with ID {} does not appear to be a canvas",
-                canvas_id
-            ))
-        })
 }
 
 fn create_context(canvas: &HtmlCanvasElement) -> Result<WebGl2RenderingContext, JsValue> {
@@ -185,9 +152,8 @@ fn setup_vao(gl: &WebGl2RenderingContext, shader_program: &WebGlProgram) -> WebG
 }
 
 impl Renderer {
-    pub fn new(canvas_id: &str) -> Result<Self, JsValue> {
-        let canvas: HtmlCanvasElement = get_canvas(canvas_id)?;
-        let gl: WebGl2RenderingContext = create_context(&canvas)?;
+    pub fn new(canvas: &HtmlCanvasElement) -> Result<Self, JsValue> {
+        let gl: WebGl2RenderingContext = create_context(canvas)?;
         let shaders: WebGlProgram = setup_shaders(&gl)?;
         let view_transform_location: WebGlUniformLocation = gl
             .get_uniform_location(&shaders, "view_transform")
@@ -198,94 +164,47 @@ impl Renderer {
 
         // Create instance
         Ok(Renderer {
-            canvas,
             gl,
             shaders,
-            canvas_to_gl_scale_x: 0.0,
-            canvas_to_gl_scale_y: 0.0,
-            scale: 1.0,
-            // TODO(Menno 28.04.2025) This offset value seems arbitrary, it might need to be dynamic
-            translation: Vec2 { x: -0.97, y: -0.97 },
-            should_update_view_transform: true,
             view_transform_location,
             vao,
             vertex_count: 0,
         })
     }
 
-    pub fn set_data(&mut self, arrangement: &Arrangement) {
+    pub fn set_data(&mut self, data: &js_sys::Float32Array) {
         self.gl.bind_vertex_array(Some(&self.vao));
-
-        // TODO(Menno 24.04.2025) Double check if this data needs to be backed for the lifetime of
-        //  the buffer, or only for the buffer_data call
-        let vertices_array = unsafe { js_sys::Float32Array::view(&arrangement.points) };
-
         self.gl.buffer_data_with_array_buffer_view(
             WebGl2RenderingContext::ARRAY_BUFFER,
-            &vertices_array,
+            &data,
             WebGl2RenderingContext::DYNAMIC_DRAW,
         );
-
-        self.vertex_count = (arrangement.points.len() / 6) as i32;
+        self.vertex_count = (data.length() / 6) as i32;
         self.gl.bind_vertex_array(None);
     }
 
-    pub fn resize(&mut self) {
-        let width = self.canvas.client_width();
-        let height = self.canvas.client_height();
-        self.canvas_to_gl_scale_x = 2.0 / width as f32;
-        self.canvas_to_gl_scale_y = 2.0 / height as f32;
-        self.canvas.set_width(width as u32);
-        self.canvas.set_height(height as u32);
+    pub fn set_viewport(&mut self, width: i32, height: i32) {
         self.gl.viewport(0, 0, width, height);
     }
 
-    fn update_view_transform(&mut self) {
-        let view_transform: Mat3 = Mat3::from_scale_angle_translation(
-            // TODO(Menno 28.04.2025) this scale should be adapted automatically by the layout size
-            Vec2 {
-                x: self.scale * 0.019,
-                y: self.scale * 0.019,
-            },
-            0.0,
-            self.translation,
-        );
-
+    pub fn draw(&mut self, view_transform: &[f32; 9]) {
+        // Prepare state
         self.gl.use_program(Some(&self.shaders));
+        self.gl.bind_vertex_array(Some(&self.vao));
+
+        // Update uniforms
         self.gl.uniform_matrix3fv_with_f32_array(
             Some(&self.view_transform_location),
             false,
-            &view_transform.to_cols_array(),
+            view_transform,
         );
-        self.gl.use_program(None);
-        self.should_update_view_transform = false;
-    }
 
-    pub fn accumulate_scale(&mut self, delta_scale: f32) {
-        let scaled_delta_scale = delta_scale * self.canvas_to_gl_scale_y * SCALE_SPEED_FACTOR;
-        self.scale = (self.scale + scaled_delta_scale).clamp(1.0, 5.0);
-        self.should_update_view_transform = true;
-    }
-
-    pub fn accumulate_translation(&mut self, delta_x: f32, delta_y: f32) {
-        let scaled_delta_x = delta_x * self.canvas_to_gl_scale_x;
-        let scaled_delta_y = delta_y * self.canvas_to_gl_scale_y;
-        // TODO(Menno 28.04.2025) The translation should be applied in a different coordinate system
-        self.translation.x = (self.translation.x + scaled_delta_x).clamp(-2.0, 0.0);
-        self.translation.y = (self.translation.y + scaled_delta_y).clamp(-2.0, 0.0);
-        self.should_update_view_transform = true;
-    }
-
-    pub fn draw(&mut self) {
-        if self.should_update_view_transform {
-            self.update_view_transform();
-        }
-
+        // Clear screen and draw points
         self.gl.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
-        self.gl.use_program(Some(&self.shaders));
-        self.gl.bind_vertex_array(Some(&self.vao));
         self.gl
             .draw_arrays(WebGl2RenderingContext::POINTS, 0, self.vertex_count);
+
+        // Reset state
         self.gl.bind_vertex_array(None);
         self.gl.use_program(None);
     }
