4b8efd605fba0e8aea91b4f22e5bfbca110c658b
Menno van der Graaf <mennovandergraaf@hotmail.com>
2025-05-24 00:20:30 +0200

    Add animation frame scheduling to rust lib

---
+++ src/frame_scheduler.rs
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: 2025 Menno van der Graaf <mennovandergraaf@hotmail.com>
+// SPDX-License-Identifier: MIT
+
+use std::cell::Cell;
+use std::rc::Rc;
+use std::time::Duration;
+use wasm_bindgen::closure::Closure;
+use wasm_bindgen::{JsCast, JsValue};
+use web_sys::Window;
+
+pub type OnFrameCb = dyn FnMut(Duration);
+type OnFrameCbInternal = dyn FnMut(f64);
+
+pub struct FrameScheduler {
+    window: Window,
+    on_frame_closure: Closure<OnFrameCbInternal>,
+    frame_request_id: i32,
+    frame_requested: Rc<Cell<bool>>,
+}
+
+impl FrameScheduler {
+    pub fn new(mut on_frame_cb: Box<OnFrameCb>) -> Result<Self, JsValue> {
+        let frame_requested = Rc::new(Cell::new(false));
+
+        let frame_requested_clone = frame_requested.clone();
+        let on_frame_closure = Closure::wrap(Box::new(move |timestamp: f64| {
+            frame_requested_clone.set(false);
+
+            // The DOMHighResTimeStamp is in milliseconds, convert it to a std time Duration
+            let timestamp = Duration::from_micros((timestamp * 1000.0) as u64);
+            on_frame_cb(timestamp);
+        }) as Box<OnFrameCbInternal>);
+
+        Ok(Self {
+            window: web_sys::window().ok_or(JsValue::from_str("Unable to access the window"))?,
+            on_frame_closure,
+            frame_request_id: 0,
+            frame_requested,
+        })
+    }
+
+    pub fn schedule(&mut self) -> Result<(), JsValue> {
+        if self.frame_requested.get() {
+            // A frame request is already pending.
+            return Ok(());
+        }
+        self.frame_request_id = self
+            .window
+            .request_animation_frame(self.on_frame_closure.as_ref().unchecked_ref())?;
+        self.frame_requested.set(true);
+        Ok(())
+    }
+
+    pub fn _cancel(&self) {
+        self.window
+            .cancel_animation_frame(self.frame_request_id)
+            .unwrap();
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,47 +2,54 @@
 // SPDX-License-Identifier: MIT
 
 mod board;
+mod frame_scheduler;
 mod graph;
 mod graph_view;
 mod solver;
+mod utils;
 
 use crate::board::BoardId;
 use crate::graph::Node;
 use crate::graph_view::GraphView;
 use crate::solver::Solver;
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct WiggersGraaf {
     solver: Solver,
-    graph_view: GraphView,
+    graph_view: Rc<RefCell<GraphView>>,
 }
 
 #[wasm_bindgen]
 impl WiggersGraaf {
     #[wasm_bindgen(constructor)]
-    pub fn new(canvas_id: &str) -> Result<Self, JsValue> {
+    pub fn new(meta_canvas_id: &str) -> Result<Self, JsValue> {
         console_error_panic_hook::set_once();
         env_logger::init();
         Ok(Self {
             solver: Solver::new(),
-            graph_view: GraphView::new(canvas_id)?,
+            graph_view: GraphView::new(meta_canvas_id)?,
         })
     }
-    pub fn draw(&mut self) {
-        self.graph_view.draw();
+    pub fn draw(&mut self) -> Result<(), JsValue> {
+        self.graph_view.borrow_mut().schedule_draw()
     }
 
     pub fn resize_meta_canvas(&mut self) {
-        self.graph_view.resize();
+        self.graph_view.borrow_mut().resize();
     }
 
     pub fn accumulate_translation(&mut self, delta_x: f32, delta_y: f32) {
-        self.graph_view.accumulate_translation(delta_x, delta_y);
+        self.graph_view
+            .borrow_mut()
+            .accumulate_translation(delta_x, delta_y);
     }
 
     pub fn accumulate_zoom(&mut self, zoom_movement: f32, target_x: f32, target_y: f32) {
         self.graph_view
+            .borrow_mut()
             .accumulate_zoom(zoom_movement, target_x, target_y);
     }
 
@@ -55,6 +62,8 @@ impl WiggersGraaf {
     }
 
     pub fn set_active_state(&mut self, active_state: BoardId) {
-        self.graph_view.set_data(&self.solver.graph, active_state);
+        self.graph_view
+            .borrow_mut()
+            .set_data(&self.solver.graph, active_state);
     }
 }
